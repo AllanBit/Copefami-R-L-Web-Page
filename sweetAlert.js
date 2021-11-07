7164e4d0913c373f0b8a8c587c5d9b27ced15809
AllanBit <79170244+AllanBit@users.noreply.github.com>
2021-11-07 17:53:18 -0600

    Add files via upload

---
+++ sweetAlert.js
@@ -0,0 +1,39 @@
+ //if(!localStorage.getItem('User')){
+    
+    Swal.fire({
+      
+   
+    title:"¡Bienvenido!",
+    
+    text: 'Te invitamos a probar las características de la nueva "Copefami Web". ',
+    
+    //icon:'info',
+    
+    footer: "Developed by Allan Lacán",
+    
+    timer:30000,
+    
+    timerProgressBar: true,
+    
+    backdrop:true,
+    
+    allowEnterKey:true,
+    
+    allowOutsideClick:true,
+    
+    allowEscapeKey:true,
+    
+    toast: false,
+    
+    imageUrl: "img/logo1.png",
+    
+    confirmButtonText: "Aceptar"
+  
+    })
+     
+    //localStorage.setItem('User', true);
+    
+    
+    
+// }
+
